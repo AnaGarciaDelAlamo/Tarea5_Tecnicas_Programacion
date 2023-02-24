ec7df08515031fccfd6916fe3388ba577d07ff74
ana <andelamo@myuax.com>
2023-02-24 19:18:33 +0100

    ejercicios continuacion

--- src/main/java/Ejercicio4.java
+++ src/main/java/Ejercicio4.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ejercicio4 {
-    /*Cree un algoritmo que pida a los usuarios que voten (los usuarios se turnarán para usar su máquina de votación). Se proponen varios candidatos. Al final del día, el presidente de la mesa de votación introduce un código especial para finalizar la votación (68753421) y se muestran los resultados. Eso sí, este ejercicio es teórico, ya que la seguridad no está garantizada y un usuario puede votar varias veces...
+   /*Cree un algoritmo que pida a los usuarios que voten (los usuarios se turnarán para usar su máquina de votación). Se proponen varios candidatos. Al final del día, el presidente de la mesa de votación introduce un código especial para finalizar la votación (68753421) y se muestran los resultados. Eso sí, este ejercicio es teórico, ya que la seguridad no está garantizada y un usuario puede votar varias veces...
 
 Ejemplo de ejecución del algoritmo:
 
@@ -13,18 +13,18 @@ Elija entre:
 
 3 - María AGUAVIVA
 
-4 - Nadia LETUX
+4 - Nadia LETUX*/
 
-4*/
-
-
-
-
-
-
-
-
-
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] tabla = new int[4];
+        int valor = 0;
+        while (valor != 68753421) {
+            System.out.println("Elija entre: ");
+            System.out.println("1 - Emilia TERREROS");
+            System.out.println("2 - Gerardo ESCODAR");
+            System.out.println("3 - María AGUAVIVA");
+            System.out.println("4 - Nadia LETUX");
+        }
+    }
 }
--- src/main/java/Ejercicio8.java
+++ src/main/java/Ejercicio8.java
@@ -1,2 +1,58 @@
+
+import java.util.Scanner;
 public class Ejercicio8 {
+    /*Realizar un algoritmo que permita jugar a los barcos con las siguientes simplificaciones:
+
+Solo hay un jugador.
+
+Solo hay cuatro filas y cuatro columnas.
+
+Solo hay un barco que ocupa una única celda (este espacio se elige al azar).
+
+Ejemplo de ejecución:
+
+????
+
+????
+
+????
+
+????
+
+¿Qué columna? 2
+*/
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int TAMAÑO = 4;
+        int i, j;
+        int[][] tab2d = new int[TAMAÑO][TAMAÑO];
+
+
+        for (j = 0; j < TAMAÑO; j++) {
+            for (i = 0; i < TAMAÑO; i++) {
+                System.out.print(tab2d[j][i] + "?");
+            }
+            System.out.println();
+
+
+            System.out.println("¿Qué columna? ");
+            int columna = sc.nextInt();
+            System.out.println("¿Qué fila? ");
+            int fila = sc.nextInt();
+            if (tab2d[fila][columna] == 0) {
+                System.out.println("Agua");
+                System.out.println(tab2d[fila][columna]);
+            } else {
+                System.out.println("Tocado");
+                System.out.println(tab2d[fila][columna]);
+            }
+
+            if (tab2d[fila][columna] == 1) {
+                System.out.println("Hundido");
+                System.out.println(tab2d[fila][columna]);
+            }
+            System.out.println();
+        }
+    }
 }
