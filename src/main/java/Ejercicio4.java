3c6a0f935665d8621a3c60b04a918a1c26f5213f
ana <andelamo@myuax.com>
2023-02-25 13:42:20 +0100

    ejercicio4 fin

--- src/main/java/Ejercicio4.java
+++ src/main/java/Ejercicio4.java
@@ -18,13 +18,46 @@ Elija entre:
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] tabla = new int[4];
-        int valor = 0;
-        while (valor != 68753421) {
+        int valor;
+        try{
+        do{
+
             System.out.println("Elija entre: ");
             System.out.println("1 - Emilia TERREROS");
             System.out.println("2 - Gerardo ESCODAR");
             System.out.println("3 - María AGUAVIVA");
             System.out.println("4 - Nadia LETUX");
+
+            valor = sc.nextInt();
+
+            switch (valor){
+                case 1:
+                    System.out.println("Ha votado a Emilia TERREROS");
+                    tabla[0]++;
+                    break;
+                case 2:
+                    System.out.println("Ha votado a Gerardo ESCODAR");
+                    tabla[1]++;
+                    break;
+                case 3:
+                    System.out.println("Ha votado a María AGUAVIVA");
+                    tabla[2]++;
+                    break;
+                case 4:
+                    System.out.println("Ha votado a Nadia LETUX");
+                    tabla[3]++;
+                    break;
+            }
+
+
+        }while (valor != 68753421);
+        }catch (Exception e){
+            System.out.println("Ha introducido un valor incorrecto");
         }
+        System.out.println("Ha finalizado la votación");
+        System.out.println("Emilia TERREROS ha obtenido " + tabla[0] + " votos");
+        System.out.println("Gerardo ESCODAR ha obtenido " + tabla[1] + " votos");
+        System.out.println("María AGUAVIVA ha obtenido " + tabla[2] + " votos");
+        System.out.println("Nadia LETUX ha obtenido " + tabla[3] + " votos");
     }
 }
