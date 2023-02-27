e6f0c4c212ec1c6cae7827432b4676391f137862
ana <andelamo@myuax.com>
2023-02-27 21:46:12 +0100

    ejercicio8 acabado

--- src/main/java/Ejercicio8.java
+++ src/main/java/Ejercicio8.java
@@ -1,69 +1,51 @@
 
 import java.util.Scanner;
+
 public class Ejercicio8 {
-    /*Realizar un algoritmo que permita jugar a los barcos con las siguientes simplificaciones:
 
-Solo hay un jugador.
+    static Scanner sc = new Scanner(System.in);
 
-Solo hay cuatro filas y cuatro columnas.
+    public static void juego() {
+        char[][] tabla = new char[4][4];
+        int fila = (int) (Math.random() * 4);
+        int columna = (int) (Math.random() * 4);
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[i].length; j++) {
+                tabla[i][j] = '?';
+            }
+        }
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla[i].length; j++) {
+                System.out.print(tabla[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int filaUsuario = 0;
+        int columnaUsuario = 0;
+        do {
+            System.out.println("¿Qué columna?");
+            columnaUsuario = sc.nextInt() - 1;
+            System.out.println("¿Qué fila?");
+            filaUsuario = sc.nextInt() - 1;
+            if (filaUsuario == fila && columnaUsuario == columna) {
+                System.out.println("Tocado y hundido");
 
-Solo hay un barco que ocupa una única celda (este espacio se elige al azar).
-
-Ejemplo de ejecución:
-
-????
-
-????
-
-????
-
-????
-
-¿Qué columna? 2
-*/
+            } else {
+                tabla[filaUsuario][columnaUsuario] = '~';
+                System.out.println("Agua");
+            }
+            for (int i = 0; i < tabla.length; i++) {
+                for (int j = 0; j < tabla[i].length; j++) {
+                    System.out.print(tabla[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } while (filaUsuario != fila || columnaUsuario != columna);
+        System.out.println("Ha ganado");
+    }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] tab2d = new int[4][4];
-        for(int i = 0; i < tab2d.length; i++) {
-            for(int j = 0; j < tab2d[i].length; j++) {
-                tab2d[i][j] = Math.random() < 0.5 ? 0 : 1;
-            }
-        }
-
-        for(int i = 0; i < tab2d.length; i++) {
-            for(int j = 0; j < tab2d[i].length; j++) {
-                System.out.print(tab2d[i][j]);
-            }
-            System.out.println();
-        }
-        boolean terminar = false;
-
-        do{
-
-            System.out.println("¿Qué columna? ");
-            int columna = sc.nextInt();
-            System.out.println("¿Qué fila? ");
-            int fila = sc.nextInt();
-            if (tab2d[fila][columna] == 0) {
-                System.out.println("Agua");
-                System.out.println(tab2d[fila][columna]);
-                terminar = false;
-            } else {
-                System.out.println("Tocado");
-                System.out.println(tab2d[fila][columna]);
-                terminar = false;
-            }
-
-            if (tab2d[fila][columna] == 1) {
-                System.out.println("Hundido");
-                System.out.println("Ha ganado");
-                System.out.println(tab2d[fila][columna]);
-                terminar = true;
-            }
-            System.out.println();
-
-        }while(terminar == false);
-
+        Ejercicio8 juego = new Ejercicio8();
+        juego.juego();
     }
 }
